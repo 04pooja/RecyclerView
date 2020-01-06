de8e12938195b72b5af63d0ced58a9dfc81edace
pooja <36655807+04pooja@users.noreply.github.com>
2020-01-06 21:30:17 +0530

    My recycler view

---
+++ MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.recycler;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference reference;
+    RecyclerView recyclerView;
+    ArrayList<Profile>list;
+    MyAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list=new ArrayList<>();
+
+        recyclerView=(RecyclerView)findViewById(R.id.myRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        reference= FirebaseDatabase.getInstance().getReference().child("Profiles");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    Profile p=dataSnapshot1.getValue(Profile.class);
+                    list.add(p);
+                }
+                adapter=new MyAdapter(MainActivity.this,list);
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this,"opsss.....error",Toast.LENGTH_LONG);
+
+            }
+        });
+    }
+}
